7ddefa1c7fbcaf63633c8cc5c840b4938c64237b
DiegoSouzaInteliger <diego.souza@belgari.com.br>
2021-03-14 17:27:40 -0300

    Adicionado arquivos iniciais para realizar o login e cadastro de ususario

---
+++ aplicacao/views.py
@@ -0,0 +1,10 @@
+from rest_framework import generics
+
+from .models import Usuario
+from .serializers import UsuarioSerializer
+
+
+class UsuarioList(generics.ListCreateAPIView):
+
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
